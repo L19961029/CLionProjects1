01fceb2f2e1a6d3410e8fbf24938339f0ad35e47
正义碳 <1633100573@qq.com>
2019-08-20 18:22:11 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#define error 0
+#include "49test.h"
+//#include "49test.h"
+#include "method.h"
+using namespace std;
+
+
+int a=0;
+
+int main(){
+
+}
