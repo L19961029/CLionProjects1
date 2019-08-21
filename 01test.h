494a65fa5b239a7f517d9dc41840b3f9f049ede1
正义碳 <1633100573@qq.com>
2019-08-20 21:41:43 -0400

    Create 01test.h

---
+++ 01test.h
@@ -0,0 +1,16 @@
+//
+// Created by 正义碳 on 2019-06-21.
+//
+
+#ifndef UNTITLED1_01TEST_H
+#define UNTITLED1_01TEST_H
+
+#endif //UNTITLED1_01TEST_H
+
+using namespace std;
+
+void ljy1(){
+    int a=3;
+    cout<<"b"<<endl;
+}
+
